52bcf3be4e5c18f974cf718238490f10f41312ea
Clara <144264170+clara-tyman@users.noreply.github.com>
2023-12-06 12:36:36 -0500

    readme and sonar check

--- main.ts
+++ main.ts
@@ -1,8 +1,22 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Clara
+ * Created on: Dec 2023
+ * This program uses the bluetooth radios
 */
 
-basic.showString('Hello, World!')
+// setup
+radio.setGroup(1)
+basic.showIcon(IconNames.Happy)
+
+let distanceToObject: number = 0
+
+// find distance from sonar
+input.onButtonPressed(Button.A, function () {
+  basic.clearScreen()
+  distanceToObject = sonar.ping(
+  DigitalPin.P1,
+  DigitalPin.P2,
+  PingUnit.Centimeters
+  )
+})
