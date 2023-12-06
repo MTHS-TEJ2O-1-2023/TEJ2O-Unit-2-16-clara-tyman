acda1ab16c89a6f52ab182d1bd246ce93e809f22
Clara <144264170+clara-tyman@users.noreply.github.com>
2023-12-06 13:24:38 -0500

    fixs

--- main.ts
+++ main.ts
@@ -11,22 +11,26 @@ basic.showIcon(IconNames.Happy)
 
 let distanceToObject: number = 0
 
-// find distance from sonar
+//loop
+while (true) {
 if (input.buttonIsPressed(Button.A) === true) {
-  basic.clearScreen()
-  distanceToObject = sonar.ping(
-    DigitalPin.P1,
-    DigitalPin.P2,
-    PingUnit.Centimeters
-  )
-}
+  distanceToObject = sonar.ping(DigitalPin.P0, DigitalPin.P1, PingUnit.Centimeters)
+
+// if distance is less than or equal to 10
 if (distanceToObject <= 10) {
   basic.showNumber(distanceToObject)
   basic.pause(1000)
-  radio.sendString('Too Close')
-if (distanceToObject <= 10) {
-    basic.showNumber(distanceToObject)
-    basic.pause(1000)
-    radio.sendString('Good')
+  radio.sendString('Too close')
+  basic.pause(1000)
+  basic.showIcon(IconNames.Happy)
+}
+// if distance is more than 10
+if (distanceToObject > 10) {
+  basic.showNumber(distanceToObject)
+  basic.pause(1000)
+  radio.sendString("Ok")
+  basic.pause(1000)
+  basic.showIcon(IconNames.Happy)
+    }
   }
 }
