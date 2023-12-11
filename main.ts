b810704f2f61b47022e6af1e974d17d3c62ece6d
Clara <144264170+clara-tyman@users.noreply.github.com>
2023-12-11 13:07:29 -0500

    indentation fix

--- main.ts
+++ main.ts
@@ -11,24 +11,24 @@ basic.showIcon(IconNames.Happy)
 
 let distanceToObject: number = 0
 
-//loop
+// loop
 while (true) {
-if (input.buttonIsPressed(Button.A) === true) {
-  distanceToObject = sonar.ping(DigitalPin.P1, DigitalPin.P2, PingUnit.Centimeters)
+  if (input.buttonIsPressed(Button.A) === true) {
+    distanceToObject = sonar.ping(DigitalPin.P1, DigitalPin.P2, PingUnit.Centimeters)
 
-// if distance is less than or equal to 10
-if (distanceToObject <= 10) {
-  basic.showNumber(distanceToObject)
-  basic.pause(1000)
-  radio.sendString('Too close')
-  basic.pause(1000)
-}
-// if distance is more than 10
-if (distanceToObject > 10) {
-  basic.showNumber(distanceToObject)
-  basic.pause(1000)
-  radio.sendString("Ok")
-  basic.pause(1000)
+    // if distance is less than or equal to 10
+    if (distanceToObject <= 10) {
+      basic.showNumber(distanceToObject)
+      basic.pause(1000)
+      radio.sendString('Too close')
+      basic.pause(1000)
+    }
+    // if distance is more than 10
+    if (distanceToObject > 10) {
+      basic.showNumber(distanceToObject)
+      basic.pause(1000)
+      radio.sendString('Ok')
+      basic.pause(1000)
     }
   }
 }
